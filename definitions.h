59a1ca36c21e330811fea09c151ce07da26eabc6
Asseri Lintanen <asseri.lintanen@hedengren.fi>
2019-02-27 09:42:16 +0200

    add uint? defines

--- definitions.h
+++ definitions.h
@@ -1,5 +1,9 @@
 #define NULL 0
 
+#define uint unsigned int
+#define int unsigned int
+#define intv int
+
 #define uint64_t int
 #define uint56_t int
 #define uint48_t int
@@ -8,7 +12,6 @@
 #define uint24_t int
 #define uint16_t int
 #define uint8_t int
-#define uint unsigned int
 
 #define int64_t int
 #define int56_t int
@@ -19,19 +22,27 @@
 #define int16_t int
 #define int8_t int
 
-#define int64 int
-#define int56 int
-#define int48 int
-#define int40 int
-#define int32 int
-#define int24 int
-#define int16 int
-#define int8 int
-#define intv int
+#define uint64 uint64_t
+#define uint56 uint56_t
+#define uint48 uint48_t
+#define uint40 uint40_t
+#define uint32 uint32_t
+#define uint24 uint24_t
+#define uint16 uint16_t
+#define uint8 uint8_t
+
+#define int64 int64_t
+#define int56 int56_t
+#define int48 int48_t
+#define int40 int40_t
+#define int32 int32_t
+#define int24 int24_t
+#define int16 int16_t
+#define int8 int8_t
 
 #define variable unsigned
-#define cstring char
-#define string char
+#define cstring char*
+#define string char*
 #define nybble char
 #define reversed unsigned
 #define bitfield int
